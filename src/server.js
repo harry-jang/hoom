10958f77c5c4d7804d8202f779a45ed0a36c1157
Harry Jang <harry.jang@kakao.com>
2023-08-14 23:44:21 +0900

    개발용 디폴트 세팅

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ app.use("/public", express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => res.render("home"));
 
+// 홈 url외에 다른 url로 호출이 왔을 경우 home으로 리다이렉트 시킴
+app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 app.listen(3000);
