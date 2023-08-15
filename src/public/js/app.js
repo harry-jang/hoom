5a483694494e8b7111b2dd32a751a6488970f8b1
Harry Jang <harry.jang@kakao.com>
2023-08-15 13:02:46 +0900

    닉네임 기능 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,12 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const messageForm = document.querySelector('#message');
+const nicknameForm = document.querySelector('#nick');
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server ✅");
@@ -20,7 +26,17 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("chat", input.value));
     input.value = "";
 }
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nicknameForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+
+    input.value = "";
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -23,12 +23,21 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "익명";
     console.log("Connected to Browser ✅");
     socket.on("close", () => {
         console.log("Disconnected from the Browser ❌");
     });
-    socket.on("message", (message) => {  
-        sockets.forEach(aSocket => aSocket.send(message.toString()));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+
+        switch(message.type) {
+            case "chat" :
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
     });
 });
 
