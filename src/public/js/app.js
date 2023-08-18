5b7e59d4a2e23fd71b85d5beb187fd8f2087bbec
sage319 <sage319@netmarble.com>
2023-08-19 00:48:36 +0900

    비디오 작업 중간저장

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,87 +1,21 @@
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const roomNameForm = welcome.querySelector("#room_name");
-const nameForm = welcome.querySelector("#name");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
 
-room.hidden = true;
 
-let roomName;
+let myStream;
 
-function addMessage(message) {
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = message;
-    ul.appendChild(li);
-}
-
-function showRoom(userCount) {
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${userCount})`;
-    const msgForm = room.querySelector("#msg");
-    
-    msgForm.addEventListener("submit", handleMessageSubmit)
-}
-
-function handleMessageSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const value = input.value;
-    socket.emit("chat_message", input.value, roomName, () => {
-        addMessage(`You: ${value}`);
-    });
-    input.value = "";  
-}
-
-function handleNicknameSubmit(event) {
-    event.preventDefault();
-    const input = welcome.querySelector("#name input");
-    socket.emit("nickname", input.value);
-}
-
-function handleRoomSubmit(event) {
-    event.preventDefault();
-    const input = roomNameForm.querySelector("input");
-    socket.emit("enter_room", input.value , showRoom);
-    roomName = input.value;
-    input.value  = "";
-}
-
-function refreshRoomTitle(newCount) {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-}
-
-roomNameForm.addEventListener("submit" , handleRoomSubmit);
-nameForm.addEventListener("submit", handleNicknameSubmit)
-
-socket.on("welcome", (nickname, newCount) => {
-    refreshRoomTitle(newCount);
-    addMessage(`${nickname} joined!`);
-})
-
-socket.on("bye", (nickname, newCount) => {
-    refreshRoomTitle(newCount);
-    addMessage(`${nickname} leaved!`);
-})
-
-socket.on("room_change", (rooms) => {
-    const roomList = welcome.querySelector("ul");
-    roomList.innerHTML = "";
-    if(rooms.length === 0) {
-        return;
+async function getMedia() {
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia(
+            {
+                audio:true,
+                video:true,
+            }
+        );
+    } catch(e) {
+        console.log(e)
     }
+}
 
-    rooms.forEach(room => {
-        const li = document.createElement("li");
-        li.innerText = room;
-        roomList.append(li);
-    });
-});
-
-// (msg) =>{addMessage(msg)} === addMessage
-socket.on("chat_message", addMessage);
-
+getMedia()
